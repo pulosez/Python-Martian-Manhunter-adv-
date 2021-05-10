7a35b6169482f3eba5cc84c206f808b2da748448
lluzhnyi <liubomyr@cozymeal.com>
2021-05-10 21:02:25 +0300

    tests

---
+++ lectures/tests/unittest/test_classes.py
@@ -0,0 +1,13 @@
+import unittest
+from classes import ClassForTest
+
+class TestForClasses(unittest.TestCase):
+
+    def setUp(self):
+        self.classTest = ClassForTest(2, 4)
+
+    def test_class_dif(self):
+        self.assertEqual(self.classTest.check_dif(), -2)
+
+    def test_class_sum(self):
+        self.assertEqual(self.classTest.check_sum(), 6)

347b8aca47f9105667c4cdab1f2800e8854dd968
lluzhnyi <liubomyr@cozymeal.com>
2021-06-10 22:26:36 +0300

    flask 3 homework

--- lectures/flask/app.py
+++ lectures/flask/app.py
@@ -1,5 +1,6 @@
 # flask_web/app.py
 from flask import Flask, render_template, request, jsonify, Response
+from flask_restful import Resource, Api
 import requests
 from config import Config
 
@@ -29,6 +30,43 @@ def search_weather():
         return render_template("weather.html", weather=weather)
 
     return Response(status=404)
+
+
+api = Api(app)
+
+todos = {}
+
+
+class Todo(Resource):
+
+    def get(self, todo_id):
+        try:
+            data = {todo_id: todos[todo_id]}
+        except KeyError:
+            return Response("Not found", status=404)
+        return data
+
+    def put(self, todo_id):
+        todos[todo_id] = request.json.get('text')
+        return {todo_id: todos[todo_id]}
+
+    def delete(self, todo_id):
+        del todos[todo_id]
+        return Response(todos, status=204)
+
+
+class TodoList(Resource):
+
+    def get(self):
+        return todos
+
+    def post(self):
+        todos[request.json.get('todo_id', None)] = request.json.get('text', "")
+        return todos
+
+
+api.add_resource(Todo, '/todos/<int:todo_id>')
+api.add_resource(TodoList, '/todos')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- lectures/flask/tests/conftest.py
+++ lectures/flask/tests/conftest.py
@@ -10,3 +10,11 @@ from app import app
 def client():
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def todos():
+    yield {
+        "todo_id": 1,
+        "text": "text"
+    }

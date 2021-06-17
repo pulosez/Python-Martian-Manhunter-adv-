901413b908fc96f6859d83b2cfd3cb07aca60f7f
lluzhnyi <liubomyr@cozymeal.com>
2021-06-17 21:00:45 +0300

    flask 5 homework

--- lectures/flask/routes/blog.py
+++ lectures/flask/routes/blog.py
@@ -1,12 +1,20 @@
-from app import app, api
-from flask import render_template, request
+from app import app, api, db
+from flask import render_template, request, Response
 from config import Config, articles
 from flask_restful import Resource, Api
+from models.models import Article, User
 
 
 @app.route('/', methods=["GET"])
 def homepage():
-    return render_template('blog/index.html', config=Config, articles=articles())
+    articles = Article.query.all()
+    return render_template('blog/index.html', config=Config, articles=articles)
+
+
+@app.route('/article/<string:slug>')
+def article_details(slug):
+    article = Article.query.filter_by(slug=slug).first()
+    return render_template('blog/details.html', article=article)
 
 
 class MenuItem(Resource):
@@ -17,5 +25,59 @@ class MenuItem(Resource):
         }
 
 
+class Articles(Resource):
+    def post(self):
+        data = request.json
+        article = Article(
+            title=data.get('title'),
+            slug=data.get('slug'),
+            author_id=data.get('author_id'),
+            description=data.get('description'),
+            short_description=data.get('short_description'),
+            img=data.get('img')
+        )
+        db.session.add(article)
+        db.session.commit()
+        return article.serialize
+
+    def get(self):
+        articles = Article.query
+        if request.args.get('title'):
+            articles = articles.filter_by(title=request.args.get('title'))
+
+        # articles = articles.filter(Article.title.startswith('A'))
+
+        if request.args.get("sort_by"):
+            articles = articles.order_by(request.args.get("sort_by"))
+
+        articles = articles.all()
+        serialized_articles = []
+        for article in articles:
+            serialized_articles.append(article.serialize)
+
+        return serialized_articles
+
+
+class ArticlesEntity(Resource):
+    def get(self, id):
+        article = Article.query.get(id)
+        if article == None:
+            return Response(status=404)
+        return article.serialize
+
+
+class Users(Resource):
+    def get(self):
+        user = User.query.get(1)
+        serialized_articles = []
+        for article in user.articles:
+            print(article)
+            serialized_articles.append(article.serialize)
+        return serialized_articles
+
+
 api.add_resource(MenuItem, '/menu-items')
+api.add_resource(Articles, '/articles')
+api.add_resource(Users, '/users')
+api.add_resource(ArticlesEntity, '/articles/<int:id>')
 

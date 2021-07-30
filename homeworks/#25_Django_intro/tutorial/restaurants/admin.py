47f52b963f59345a528d9f4491be3adc40221bef
pulosez <pulosez@gmail.com>
2021-07-30 16:35:23 +0300

    Added homework 26 django orm

--- homeworks/#25_Django_intro/tutorial/restaurants/admin.py
+++ homeworks/#25_Django_intro/tutorial/restaurants/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Restaurant, Employee, Country, City, Menu, Dish
+
+admin.site.register(Restaurant)
+admin.site.register(Employee)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Menu)
+admin.site.register(Dish)
